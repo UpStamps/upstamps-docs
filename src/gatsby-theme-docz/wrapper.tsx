5166c53fe4bbffd611614369be65ac331e491404
Vitor Amaral <va.works@hotmail.com>
2020-04-15 14:58:34 +0100

    Update Rest API docs and meta tags

--- src/gatsby-theme-docz/wrapper.tsx
+++ src/gatsby-theme-docz/wrapper.tsx
@@ -7,10 +7,34 @@ const Wrapper = ({ children }: { children: React.ReactNode }) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>UpStamps Documentation</title>
+        <meta
+          name="description"
+          content="Simple feature flagging and toggle service"
+        />
+        {/* Open Graph / Facebook*/}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.upstamps.com/" />
+        <meta property="og:title" content="Upstamps" />
+        <meta
+          property="og:description"
+          content="Simple feature flagging and toggle service"
+        />
+        <meta property="og:image" content="" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="http://www.upstamps.com/" />
+        <meta property="twitter:title" content="UpStamps" />
+        <meta
+          property="twitter:description"
+          content="Simple feature flagging and toggle service"
+        />
+        <meta property="twitter:image" content="" />
+
         <link
           rel="icon"
-          type="image/png"
-          href="https://avatars.dicebear.com/v2/male/dd.svg"
+          type="image/jpg"
+          href="https://dev.upstamps.com/imgs/logo.jpg"
         />
       </Helmet>
       {children}
